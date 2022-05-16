13475ea6a4d1809f0fc830141eddd98369b32460
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-05-16 15:38:45 +0200

    Initial commit

---
+++ test/aws-serverless-training.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsServerlessTraining from '../lib/aws-serverless-training-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-serverless-training-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsServerlessTraining.AwsServerlessTrainingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
