fb262a832f0567f3f89c07fa60eac0f4d89bfb09
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-05-18 10:10:29 +0200

    get bin files into aws sdk v1

--- bin/aws-serverless-training.ts
+++ bin/aws-serverless-training.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { AwsServerlessTrainingStack } from '../lib/aws-serverless-training-stack';
 
 const app = new cdk.App();
