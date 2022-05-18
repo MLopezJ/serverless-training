c25284c36e142e0f1b9b01218ec8e65e445973ec
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-05-18 10:01:33 +0200

    define v1 as th aws cdk version to be used

--- lib/aws-serverless-training-stack.ts
+++ lib/aws-serverless-training-stack.ts
@@ -1,20 +1,16 @@
-import * as cdk from 'aws-cdk-lib'
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import s3 = require('aws-cdk-lib/aws-s3');
+import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
 
 const imageBucketName = "cdk-serverlesstraining-imgbucket"
 
 
-export class AwsServerlessTrainingStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsServerlessTrainingStack extends cdk.Stack  {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // =====================================================================================
     // Image Bucket
     // =====================================================================================
-    const bucket = new s3.Bucket(this, 'MyFirstBucket');
-
     const imageBucket = new s3.Bucket(this, imageBucketName, {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
