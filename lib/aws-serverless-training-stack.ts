943e5605f9fa4b7a5a74a1b4ac89ab303205bb72
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-05-18 10:33:33 +0200

    add lambda to the stack

--- lib/aws-serverless-training-stack.ts
+++ lib/aws-serverless-training-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from '@aws-cdk/core';
+import { Duration } from '@aws-cdk/core';
 import s3 = require('@aws-cdk/aws-s3');
 import dynamodb = require('@aws-cdk/aws-dynamodb');
+import lambda = require('@aws-cdk/aws-lambda');
+import iam = require('@aws-cdk/aws-iam');
+import event_sources = require('@aws-cdk/aws-lambda-event-sources');
 
 const imageBucketName = "cdk-serverlesstraining-imgbucket"
 
@@ -25,6 +29,31 @@ export class AwsServerlessTrainingStack extends cdk.Stack  {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     new cdk.CfnOutput(this, 'ddbTable', { value: table.tableName });
+
+    // =====================================================================================
+    // Building our AWS Lambda Function; compute for our serverless microservice
+    // =====================================================================================
+    const rekFn = new lambda.Function(this, 'rekognitionFunction', {
+      code: lambda.Code.fromAsset('rekognitionlambda'),
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      timeout: Duration.seconds(30),
+      memorySize: 1024,
+      environment: {
+          "TABLE": table.tableName,
+          "BUCKET": imageBucket.bucketName
+      },
+    });
+
+    rekFn.addEventSource(new event_sources.S3EventSource(imageBucket, { events: [ s3.EventType.OBJECT_CREATED ]}));
+    imageBucket.grantRead(rekFn);
+    table.grantWriteData(rekFn);
+
+    rekFn.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['rekognition:DetectLabels'],
+      resources: ['*']
+    }));
 
   }
 }
