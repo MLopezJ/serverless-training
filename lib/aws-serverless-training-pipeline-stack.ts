2600bf69ae8b939a93b3e33a363d3f20affcd47c
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-06-01 11:51:59 +0200

    testing cdk v2 implementation

--- lib/aws-serverless-training-pipeline-stack.ts
+++ lib/aws-serverless-training-pipeline-stack.ts
@@ -48,6 +48,7 @@ export class AwsServerlessTrainingPipelineStack extends Stack {
       authentication: SecretValue.secretsManager('serverless-training-git-access-token', {jsonField: 'serverless-training-git-access-token'})
     })
     */
+   
 
 
     const pipeline =  new CodePipeline(this, 'Pipeline', {
