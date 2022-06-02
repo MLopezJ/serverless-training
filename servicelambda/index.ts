37bfe463a93404c7576508eca82931c29dcead8f
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-06-02 16:32:35 +0200

    refactor: remove spaces and logs

--- servicelambda/index.ts
+++ servicelambda/index.ts
@@ -1,16 +1,9 @@
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+
+// Constructors for Amazon DynamoDB
 const ddbClient = new DynamoDBClient({ region: process.env.AWS_REGION });
 
-// Constructors for Amazon DynamoDB and S3 resource object
-/*
-const dynamodb = AWS.resource('dynamodb')
-const s3 = AWS.resource('s3')
-*/
-
-
 export const handler = async (event: { [x: string]: any; }, context: any) => {
-
-    console.log(' ts lambda')
 
     // Detect requested action from the Amazon API Gateway Event
     const action = event['action']
@@ -22,19 +15,15 @@ export const handler = async (event: { [x: string]: any; }, context: any) => {
 
     if (action === 'getLabels'){
         console.log('getting image labels')
-        const result = await getLabels(event); // requesting labels associate to image
-        console.log(result)
-    }
 
-    return {
-        body: JSON.stringify({message: 'Successful .ts lambda invocation'}),
-        statusCode: 200,
-      };
+        // requesting labels associate to image
+        const result = await getLabels(key);
+
+        return result
+    }
 }
 
-const getLabels = async (info: any) => {
-    const {key} = info
-
+const getLabels = async (key: any) => {
     const param = {
         TableName: process.env.TABLE,
         Key: { 'image': { S: key}}
@@ -43,16 +32,4 @@ const getLabels = async (info: any) => {
     const data = await ddbClient.send(new GetItemCommand(param));
     console.log("Success: ", data.Item);
     return data;
-}
-
-const run = async () => {
-    
-    
-  };
-
-const instanceDB = () => {
-    
-    const envName  = process.env.TABLE;
-    //const table  = dynamo.Table(envName)
-    
 }
