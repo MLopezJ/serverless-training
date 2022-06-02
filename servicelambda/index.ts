a696bb4d77f120d98d20af2576c14cac992dc549
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-06-02 11:35:45 +0200

    refactor: rename var

--- servicelambda/index.ts
+++ servicelambda/index.ts
@@ -2,8 +2,10 @@ const AWS = require("aws-sdk");
 import * as os from 'os';
 
 // Constructors for Amazon DynamoDB and S3 resource object
+/*
 const dynamodb = AWS.resource('dynamodb')
 const s3 = AWS.resource('s3')
+*/
 
 
 export const handler = (event: { [x: string]: any; }, context: any) => {
@@ -12,13 +14,13 @@ export const handler = (event: { [x: string]: any; }, context: any) => {
 
     // Detect requested action from the Amazon API Gateway Event
     const action = event['action']
-    const image = event['image']
+    const key = event['image']
 
-    if (!action || !image){
+    if (!action || !key){
         return(console.log('no info provided'))
     }
 
-    console.log(image, action)
+    console.log(key, action)
     return {
         body: JSON.stringify({message: 'Successful .ts lambda invocation'}),
         statusCode: 200,
