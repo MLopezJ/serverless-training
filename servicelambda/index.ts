009d030d26240631b05f282b1dfe46e23135e853
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-06-02 13:36:55 +0200

    refactor: add dynamo db connection to lambda

--- servicelambda/index.ts
+++ servicelambda/index.ts
@@ -1,5 +1,5 @@
-const AWS = require("aws-sdk");
-import * as os from 'os';
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+const ddbClient = new DynamoDBClient({ region: 'eu-west-1' });
 
 // Constructors for Amazon DynamoDB and S3 resource object
 /*
@@ -14,16 +14,46 @@ export const handler = (event: { [x: string]: any; }, context: any) => {
 
     // Detect requested action from the Amazon API Gateway Event
     const action = event['action']
-    const key = event['image']
+    const key = event['image'] // image
 
     if (!action || !key){
         return(console.log('no info provided'))
     }
 
-    console.log(key, action)
+    if (action === 'getLabels'){
+        console.log('getting image labels')
+        const result = getLabels(event); // requesting labels associate to image
+        console.log(result)
+    }
+
     return {
         body: JSON.stringify({message: 'Successful .ts lambda invocation'}),
         statusCode: 200,
       };
+}
 
+const getLabels = async (info: any) => {
+    const {key} = info
+    console.log(key, ' ---')
+    // instance db
+
+    const param = {
+        TableName: process.env.TABLE,
+        Key: {KEY_NAME: { N: key}}
+    }
+    const data = await ddbClient.send(new GetItemCommand(param));
+    console.log("Success", data.Item);
+    return data;
+}
+
+const run = async () => {
+    
+    
+  };
+
+const instanceDB = () => {
+    
+    const envName  = process.env.TABLE;
+    //const table  = dynamo.Table(envName)
+    
 }
