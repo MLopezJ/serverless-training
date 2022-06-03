180194c5bd6b0c498e5db81b7bdc07aeae1323ad
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-06-03 09:16:28 +0200

    refactor: temporal mock of query param

--- servicelambda/index.ts
+++ servicelambda/index.ts
@@ -24,9 +24,10 @@ export const handler = async (event: { [x: string]: any; }, context: any) => {
 }
 
 const getLabels = async (key: any) => {
+    const value = `private/eu-west-1:78ad3dad-3394-47fe-867a-2a0ddf50ba3d/photos/${key}` // temporal mock of value
     const param = {
         TableName: process.env.TABLE,
-        Key: { 'image': { S: key}}
+        Key: { 'image': { S: value}}
     }
 
     const data = await ddbClient.send(new GetItemCommand(param));
