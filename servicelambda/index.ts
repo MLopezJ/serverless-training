d677140ad4ac38f36c9b721bfebdb309ef7f9622
Jiménez López <malo@mauro-dell.nordicsemi.no>
2022-06-08 12:25:10 +0200

    refactor: comment s3 constructor

--- servicelambda/index.ts
+++ servicelambda/index.ts
@@ -5,6 +5,7 @@ import { S3Client, DeleteObjectCommand, GetObjectCommand } from "@aws-sdk/client
 const ddbClient = new DynamoDBClient({ region: process.env.AWS_REGION });
 
 // Constructor for S3
+/*
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
@@ -12,6 +13,7 @@ const s3 = new S3Client({
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
     }
 })
+*/
 
 export const handler = async (event: { [x: string]: any; }, context: any) => {
 
