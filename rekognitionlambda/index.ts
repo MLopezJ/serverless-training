37cc2c0907456c55e7e150532b036017d26c2bfc
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-06-16 11:57:32 +0200

    fix: return event as a default action on rekognition lambda

--- rekognitionlambda/index.ts
+++ rekognitionlambda/index.ts
@@ -1,7 +1,8 @@
 export const handler = async (event: { [x: string]: any; }, context: any) => {
 
     console.log("Lambda processing event: ", event)
+    console.log('work in progress')
 
-    return 'work in progress'
+    return event
     
 }
