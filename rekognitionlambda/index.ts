d9ce3b6348836c6cfff8cfb9081c0e71f616ccf0
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-06-16 16:19:17 +0200

    fix: remove logs

--- rekognitionlambda/index.ts
+++ rekognitionlambda/index.ts
@@ -3,12 +3,9 @@ export const handler = async (event: { [x: string]: any; }, context: any) => {
     console.log("Lambda processing event: ", event)
 
     const records = event.Records ?? []
-    console.log(records, typeof records)
 
     records.map((payload: { body: string; }) => {
-        console.log(typeof payload.body, payload.body)
-        const eventInformation = JSON.parse(payload.body) 
-        console.log(eventInformation.Records, typeof eventInformation.Records)
+        const eventInformation = JSON.parse(payload.body)
         eventInformation.Records.map((element: { s3: { bucket: { name: any; }; object: { key: any; }; }; }) => {
             const bucketName = element.s3.bucket.name
             const bucketKey = element.s3.object.key
