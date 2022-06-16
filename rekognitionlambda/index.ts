563694415ab8a2c3d56d982f390650b33f237b40
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-06-16 15:27:41 +0200

    fix: mock rekfunction and generateThumb methos

--- rekognitionlambda/index.ts
+++ rekognitionlambda/index.ts
@@ -1,8 +1,26 @@
 export const handler = async (event: { [x: string]: any; }, context: any) => {
 
     console.log("Lambda processing event: ", event)
-    console.log('work in progress')
+
+    const records = event.Records ?? []
+
+    records.map((payload: { body: any; }) => {
+        const eventInformation = payload.body
+        eventInformation.Records.map((element: { s3: { bucket: { name: any; }; object: { key: any; }; }; }) => {
+            const bucketName = element.s3.bucket.name
+            const bucketKey = element.s3.object.key
+            rekFunction(bucketName, bucketKey)
+            generateThumb(bucketName, bucketKey)
+        })
+    })
 
     return event
-    
+}
+
+const rekFunction = (bucket: string, key: string) => {
+    console.log('Work in progress from rekFunction ', bucket, key)
+}
+
+const generateThumb = (bucket: string, key: string) => {
+    console.log('Work in progress from generateThumb ', bucket, key)
 }
