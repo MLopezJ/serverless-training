76a8bdb6ca179478753e38bc744da0ae78c617a5
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-06-16 16:00:42 +0200

    fix: create logs for testing propouses

--- rekognitionlambda/index.ts
+++ rekognitionlambda/index.ts
@@ -3,9 +3,12 @@ export const handler = async (event: { [x: string]: any; }, context: any) => {
     console.log("Lambda processing event: ", event)
 
     const records = event.Records ?? []
+    console.log(records, typeof records)
 
-    records.map((payload: { body: any; }) => {
-        const eventInformation = payload.body
+    records.map((payload: { body: string; }) => {
+        console.log(typeof payload.body, payload.body)
+        const eventInformation = JSON.parse(payload.body) 
+        console.log(eventInformation.Records, typeof eventInformation.Records)
         eventInformation.Records.map((element: { s3: { bucket: { name: any; }; object: { key: any; }; }; }) => {
             const bucketName = element.s3.bucket.name
             const bucketKey = element.s3.object.key
