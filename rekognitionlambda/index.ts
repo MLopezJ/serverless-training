2380e1ef17592d64f09854d442a1ee4902ea2367
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-06-20 12:04:08 +0200

    refactor: creates processImage method, to do recognition function and generate thumb

--- rekognitionlambda/index.ts
+++ rekognitionlambda/index.ts
@@ -1,5 +1,6 @@
 import { DetectLabelsCommand, DetectLabelsCommandInput } from  "@aws-sdk/client-rekognition";
 import  { RekognitionClient } from "@aws-sdk/client-rekognition";
+import { SQSEvent} from 'aws-lambda'
 
 const maxLabels: number = 10
 const minConfidence: number = 50
@@ -9,43 +10,24 @@ const REGION = "eu-west-1"; //e.g. "us-east-1"
 // Create SNS service object.
 const rekogClient = new RekognitionClient({ region: REGION });
 
-export const handler = async (event: { [x: string]: any; }, context: any) => {
 
+export const handler = async (event: SQSEvent) => {
     console.log("Lambda processing event: ", event)
-
-    const records = event.Records ?? []
-    /*
-    await records.map((payload: { body: string; }) => {
+    const records = event.Records
+    for await (const payload of records) {
         const eventInformation = JSON.parse(payload.body)
-        eventInformation.Records.map(async (element: { s3: { bucket: { name: any; }; object: { key: any; }; }; }) => {
-            const bucketName = element.s3.bucket.name
-            const bucketKey = element.s3.object.key
-            await rekFunction(bucketName, bucketKey)
-            generateThumb(bucketName, bucketKey)
-        })
-    })
-    */
-
-    const payload = records[0].body
-    const eventInformation = JSON.parse(payload)
-    const bucketName = eventInformation.Records[0].s3.bucket.name
-    const bucketKey = eventInformation.Records[0].s3.object.key
-    const photo: string = replaceSubstringWithColon(bucketKey);
-    const params = {
-        Image: {
-          S3Object: {
-            Bucket: bucketName,
-            Name: photo
-          },
-        },
-        MaxLabels: maxLabels,
-        MinConfidence: minConfidence
+        for await (const element of eventInformation.Records) {
+            await processImage(element);
+        }
     }
-
-    const response = await rekogClient.send(new DetectLabelsCommand(params));
-    console.log(response)
-    console.log('!!!!!!!!!!!!!')
     return event
+}
+
+async function processImage(element: { s3: { bucket: { name: any; }; object: { key: any; }; }; }) {
+    const bucketName = element.s3.bucket.name;
+    const bucketKey = element.s3.object.key;
+    await rekFunction(bucketName, bucketKey);
+    await generateThumb(bucketName, bucketKey);
 }
 
 const rekFunction = async (bucket: string, key: string) => {
@@ -112,7 +94,7 @@ const saveLabelsInDb = (labels: (string | undefined)[] | undefined) => {
     console.log('work in progress to save recognized labels on dynamo db')
 }
 
-const generateThumb = (bucket: string, key: string) => {
+const generateThumb = async (bucket: string, key: string) => {
     console.log('Work in progress from generateThumb ', bucket, key)
 }
 
