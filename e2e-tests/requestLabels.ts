fc091b610bf6255dfce56b61e4c6f27215dffd02
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-08-15 13:13:57 +0200

    test(e2e): improve logs

--- e2e-tests/requestLabels.ts
+++ e2e-tests/requestLabels.ts
@@ -1,12 +1,17 @@
 import { CloudFormationClient } from '@aws-sdk/client-cloudformation'
+import { GetItemCommandOutput } from '@aws-sdk/client-dynamodb'
 import { stackOutput } from '@nordicsemiconductor/cloudformation-helpers'
 import { StackOutputs } from 'cdk/stacks/image-gallery'
 import { assertThat, is } from 'hamjest'
 import { stackNamePrefix } from '../cdk/stackName.js'
 import { apiGatewayClient } from './utils/apiGatewayClient'
 import { getCognitoUserCredentials } from './utils/getCognitoUserCredentials'
+import { includeKeyword } from './utils/includeKeyword.js'
+import { retry } from './utils/retry.js'
 
-const main = async () => {
+export const main = async (key: string): Promise<void> => {
+	console.log('-- Start Request Labels --')
+	const keyword = 'Shark'
 	const outputs = await stackOutput(new CloudFormationClient({}))<StackOutputs>(
 		`${stackNamePrefix}Stack`,
 	)
@@ -21,18 +26,21 @@ const main = async () => {
 	const endpoint = new URL(outputs.apiUrl)
 	const authenticatedClient = apiGatewayClient(endpoint, credentials.IdToken)
 
-	const filename = 'shark.jpg'
-	const key = `private/${credentials.IdentityId}/photos/${filename}`
-
-	const response = await authenticatedClient(
-		'images',
-		new URLSearchParams({
-			key,
-			action: 'getLabels',
-		}),
+	const response = await retry(
+		async () =>
+			authenticatedClient(
+				'images',
+				new URLSearchParams({
+					key,
+					action: 'getLabels',
+				}),
+			).then(async (response) => response.json()),
+		'Request Labels',
 	)
 
-	assertThat(response.status, is(404))
+	assertThat(
+		includeKeyword(response as GetItemCommandOutput, keyword),
+		is(true),
+	)
+	console.log('-- Finish Request Labels --')
 }
-
-main().then(console.log).catch(console.error)
