bdb8141d7fc8acc735750fc3e87a534065e84ce5
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-09-02 14:59:05 +0200

    test(e2e): deploy stack with random name

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import "source-map-support/register";
-import { stackNamePrefix } from "../cdk/stackName";
-import { ContinuousDeploymentPipelineStack } from "../cdk/stacks/continuous-deployment-pipeline";
-import { ImageGalleryStack } from "../cdk/stacks/image-gallery";
+import * as cdk from 'aws-cdk-lib'
+import 'source-map-support/register'
+import { stackNamePrefix } from '../cdk/stackName'
+import { ContinuousDeploymentPipelineStack } from '../cdk/stacks/continuous-deployment-pipeline'
+import { ImageGalleryStack } from '../cdk/stacks/image-gallery'
 
 const app = new cdk.App({
-  context: {
-    "backend.repository":
-      process.env.BACKEND_REPOSITORY ?? "MLopezJ/serverless-training",
-    "backend.branch": process.env.BACKEND_BRANCH ?? "saga",
-  },
-});
-new ImageGalleryStack(app, `${stackNamePrefix}Stack`, {});
+	context: {
+		'backend.repository':
+			process.env.BACKEND_REPOSITORY ?? 'MLopezJ/serverless-training',
+		'backend.branch': process.env.BACKEND_BRANCH ?? 'saga',
+	},
+})
+
+const stackName = app.node.tryGetContext('stackName')
+new ImageGalleryStack(app, `${stackNamePrefix}Stack`, { stackName: stackName })
 
 new ContinuousDeploymentPipelineStack(
-  app,
-  `${stackNamePrefix}PipelineStack`,
-  {}
-);
+	app,
+	`${stackNamePrefix}PipelineStack`,
+	{},
+)
